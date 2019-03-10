f8da58d85f5afeea75d1392703de3fa872a7719b
Jordi Ferrer <jferrering@gmail.com>
2019-03-10 23:03:22 +0100

    Added the DomainError class required for the callerIsEmpty exception

---
+++ src/Bingo/Shared/Domain/DomainError.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Bingo\Shared\Domain;
+
+use DomainException;
+
+/**
+ * Class DomainError
+ * @package Bingo\Shared\Domain
+ */
+abstract class DomainError extends DomainException
+{
+    /**
+     * DomainError constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct($this->errorMessage());
+    }
+
+    /**
+     * @return string
+     */
+    abstract public function errorCode(): string;
+
+    /**
+     * @return string
+     */
+    abstract protected function errorMessage(): string;
+}
